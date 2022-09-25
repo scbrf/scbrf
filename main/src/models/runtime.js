79c4abb6c3d1583155f68c7beb62a0d2854673d8
wesley <9610848@qq.com>
2022-09-25 20:05:13 +0800

    runtime and runtime unittest

--- main/src/models/runtime.js
+++ main/src/models/runtime.js
@@ -1,6 +1,9 @@
 const bunyan = require('bunyan')
 const log = bunyan.createLogger({ name: 'models/runtime' })
-const { bindIpcMainTable, emit, initDataModel } = require('../utils/events')
+const { bindIpcMainTable, initDataModel } = require('../utils/events')
+const FollowingPlanet = require('./followingPlanet')
+const Planet = require('./planet')
+const moment = require('moment')
 
 class Runtime {
   constructor() {
@@ -46,14 +49,73 @@ class Runtime {
 
     //这个模块定义了这些可以从前端传过来的事件
     bindIpcMainTable(this, [
-      { setSidebarFocus: this._onIpcSidebarFocus },
-      { setMiddleSidebarFocus: this._onIpcMiddleSidebarFocus },
+      { setSidebarFocus: this.#onIpcSidebarFocus },
+      { setMiddleSidebarFocus: this.#onIpcMiddleSidebarFocus },
     ])
   }
 
-  _onIpcSidebarFocus(focus) {}
+  /**
+   * 私有函数。 传入的参数是字符串 today unread starred 或者 my:xxxxx follow:xxxx
+   * @param {*} focus
+   */
+  #onIpcSidebarFocus(focus) {
+    let sidebarFocus = focus
+    if (focus === 'today') {
+      const articles = FollowingPlanet.following.reduce((r, p) => {
+        return [...r, ...p.articles.filter((a) => moment(a.created).isSame(moment(), 'day'))]
+      }, [])
+      articles.sort((a, b) => b.created - a.created)
+      this.set({
+        sidebarFocus,
+        middleSideBarTitle: 'Today',
+        middleSideBarArticles: articles,
+        middleSideBarFocusArticle: articles[0],
+      })
+    } else if (focus === 'unread') {
+      const articles = FollowingPlanet.following.reduce((r, p) => {
+        return [...r, ...p.articles.filter((a) => a.read === false)]
+      }, [])
+      this.set({
+        sidebarFocus,
+        middleSideBarTitle: 'Unread',
+        middleSideBarArticles: articles,
+        middleSideBarFocusArticle: articles[0],
+      })
+    } else if (focus === 'starred') {
+      const articles = FollowingPlanet.following.reduce((r, p) => {
+        return [...r, ...p.articles.filter((a) => a.starred === true)]
+      }, [])
+      this.set({
+        sidebarFocus,
+        middleSideBarTitle: 'Starred',
+        middleSideBarArticles: articles,
+        middleSideBarFocusArticle: articles[0],
+      })
+    } else if (focus.startsWith('my:')) {
+      const planet = Planet.planets.filter((a) => a.id === focus.substring('my:'.length))[0]
+      sidebarFocus = planet
+      this.set({
+        sidebarFocus,
+        middleSideBarTitle: planet.name,
+        middleSideBarArticles: planet.articles,
+        middleSideBarFocusArticle: planet.articles[0],
+      })
+    } else if (focus.startsWith('following:')) {
+      const planet = FollowingPlanet.following.filter((p) => p.id === focus.substring('following:'.length))[0]
+      sidebarFocus = planet
+      this.set({
+        sidebarFocus,
+        middleSideBarTitle: planet.name,
+        middleSideBarArticles: planet.articles,
+        middleSideBarFocusArticle: planet.articles[0],
+      })
+    }
+  }
 
-  _onIpcMiddleSidebarFocus(focus) {}
+  #onIpcMiddleSidebarFocus(focus) {
+    //focus == article.id
+    this.middleSideBarFocusArticle = this.middleSideBarArticles.filter((a) => a.id == focus)[0]
+  }
 }
 
 module.exports = new Runtime()
--- main/src/models/runtime.test.js
+++ main/src/models/runtime.test.js
@@ -1,5 +1,14 @@
+const { ipcMain } = require('electron')
+let ipcCBs = {}
+ipcMain.on.mockImplementation((name, cb) => {
+  ipcCBs[name] = cb
+})
+
 const runtime = require('./runtime')
 const { bus } = require('../utils/events')
+const FollowingPlanet = require('./followingPlanet')
+const Planet = require('./planet')
+
 test('ipfsOnline', () => {
   const cb = jest.fn()
   bus.on(runtime.events.ipfsOnlinePeersChange, cb)
@@ -73,4 +82,67 @@ test('set', () => {
   //   expect(runtime.middleSideBarArticles.length).toBe(4)
   expect(runtime.middleSideBarTitle).toBe('new title')
   expect(cb).toHaveBeenCalledTimes(1)
+})
+
+test('ipcSidebarFocus', () => {
+  runtime.middleSideBarTitle = ''
+  runtime.middleSideBarArticles = []
+  FollowingPlanet.following = [
+    {
+      id: 'test1',
+      name: 'test',
+      articles: [
+        { created: new Date().getTime(), title: 'test1' },
+        { created: new Date().getTime() - 1000 * 3600 * 24 * 5, title: 'unread', read: false },
+        { created: new Date().getTime() - 1000 * 3600 * 24 * 5, title: 'starred', starred: true },
+      ],
+    },
+  ]
+  ipcCBs['setSidebarFocus']('today')
+  expect(runtime.sidebarFocus).toBe('today')
+  expect(runtime.middleSideBarTitle).toBe('Today')
+  expect(runtime.middleSideBarArticles.length).toBe(1)
+  expect(runtime.middleSideBarArticles[0].title).toBe('test1')
+  expect(runtime.middleSideBarFocusArticle.title).toBe('test1')
+
+  ipcCBs['setSidebarFocus']('unread')
+  expect(runtime.sidebarFocus).toBe('unread')
+  expect(runtime.middleSideBarTitle).toBe('Unread')
+  expect(runtime.middleSideBarArticles.length).toBe(1)
+  expect(runtime.middleSideBarArticles[0].title).toBe('unread')
+  expect(runtime.middleSideBarFocusArticle.title).toBe('unread')
+
+  ipcCBs['setSidebarFocus']('starred')
+  expect(runtime.sidebarFocus).toBe('starred')
+  expect(runtime.middleSideBarTitle).toBe('Starred')
+  expect(runtime.middleSideBarArticles.length).toBe(1)
+  expect(runtime.middleSideBarArticles[0].title).toBe('starred')
+  expect(runtime.middleSideBarFocusArticle.title).toBe('starred')
+
+  ipcCBs['setSidebarFocus']('following:test1')
+  expect(runtime.sidebarFocus.name).toBe('test')
+  expect(runtime.middleSideBarTitle).toBe('test')
+  expect(runtime.middleSideBarArticles.length).toBe(3)
+  expect(runtime.middleSideBarArticles[0].title).toBe('test1')
+  expect(runtime.middleSideBarFocusArticle.title).toBe('test1')
+
+  Planet.planets = [
+    {
+      id: 'test2',
+      name: 'test2',
+      articles: [
+        { id: 'a1', created: new Date().getTime(), title: 'test2' },
+        { id: 'a2', created: new Date().getTime() - 1000 * 3600 * 24 * 5, title: 'unread', read: false },
+      ],
+    },
+  ]
+  ipcCBs['setSidebarFocus']('my:test2')
+  expect(runtime.sidebarFocus.name).toBe('test2')
+  expect(runtime.middleSideBarTitle).toBe('test2')
+  expect(runtime.middleSideBarArticles.length).toBe(2)
+  expect(runtime.middleSideBarArticles[0].title).toBe('test2')
+  expect(runtime.middleSideBarFocusArticle.title).toBe('test2')
+
+  ipcCBs['setMiddleSidebarFocus']('a2')
+  expect(runtime.middleSideBarFocusArticle.title).toBe('unread')
 })
