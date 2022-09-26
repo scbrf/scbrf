94a1e27ef9bb3d2e2423cc595123ab6f06f89676
wesley <9610848@qq.com>
2022-09-26 15:04:09 +0800

    remove unused code

--- main/src/controller/webview.js
+++ main/src/controller/webview.js
@@ -1,4 +1,4 @@
-const { BrowserView, ipcMain, BrowserWindow } = require('electron')
+const { BrowserView } = require('electron')
 const bunyan = require('bunyan')
 const evt = require('../utils/events')
 const log = bunyan.createLogger({ name: 'webview' })
@@ -27,29 +27,6 @@ class Webview {
       // return { action: 'deny' };
     })
     // this.view.webContents.openDevTools({mode: 'undocked'})
-    // ipcMain.on('articleFocus', async (_, p) => {
-    //   log.info('webview switch to url', p.url)
-    //   await this.view.webContents.executeJavaScript(`(()=>{
-    //                 document.body.style = "widht:100vw;height:100vh;display:flex;align-items:center;justify-content:center;";
-    //                 document.body.innerHTML = "Loading...";
-    //             })()`)
-    //   await new Promise((resolve) => setTimeout(resolve, 100))
-    //   this.view.webContents.loadURL(p.url, { userAgent: 'Planet/JS' })
-    // })
-    // bus.on('focusInfo', async (p) => {
-    //   const article = p.articles.filter((a) => a.id === p.focus)[0]
-    //   if (article) {
-    //     log.info('webview switch to url', article.url)
-    //     await this.view.webContents.executeJavaScript(`(()=>{
-    //                     document.body.style = "widht:100vw;height:100vh;display:flex;align-items:center;justify-content:center;";
-    //                     document.body.innerHTML = "Loading...";
-    //                 })()`)
-    //     await new Promise((resolve) => setTimeout(resolve, 100))
-    //     this.view.webContents.loadURL(article.url, { userAgent: 'Planet/JS' })
-    //   } else {
-    //     this.view.webContents.loadURL(`${require('../utils/websrv').WebRoot}/loading`, { userAgent: 'Planet/JS' })
-    //   }
-    // })
   }
   init() {
     this.view.setAutoResize({ height: true, width: true })
