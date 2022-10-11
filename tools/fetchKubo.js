01301de8d6d0b0df3abc3a7b52a68a310f6243dc
wesley <9610848@qq.com>
2022-10-12 01:11:21 +0800

    fix ci

--- tools/fetchKubo.js
+++ tools/fetchKubo.js
@@ -1,10 +1,13 @@
 async function fetchzip(path) {
-  const rsp = await require("axios").get(
-    "https://github.com/ipfs/kubo/releases/download/v0.15.0/kubo_v0.15.0_windows-amd64.zip",
-    {
-      responseType: "stream", // important
-    }
-  );
+  const target =
+    require("os").platform() == "darwin"
+      ? "https://github.com/ipfs/kubo/releases/download/v0.15.0/kubo_v0.15.0_darwin-arm64.tar.gz"
+      : "https://github.com/ipfs/kubo/releases/download/v0.15.0/kubo_v0.15.0_windows-amd64.zip";
+
+  console.log("need fetch", target);
+  const rsp = await require("axios").get(target, {
+    responseType: "stream", // important
+  });
   await new Promise((resolve) => {
     rsp.data.pipe(require("fs").createWriteStream(path));
     rsp.data.on("close", () => {
@@ -20,12 +23,16 @@ async function run() {
   const zip = new AdmZip("./a.zip");
   //   var zipEntries = zip.getEntries();
   //   console.log(zipEntries.map((e) => e.entryName));
+  const target =
+    require("os").platform() == "darwin" ? "kubo/ipfs" : "kubo/ipfs.exe";
+  console.log("need extract", target);
   zip.extractEntryTo(
-    "kubo/ipfs.exe",
+    target,
     require("path").join(__dirname, "..", "ipfsbin"),
     false,
     true
   );
+  console.log("done!");
 }
 
 run();
