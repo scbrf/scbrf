caba0c05370622646a0bbdfc58188d7aa578566a
wesley <9610848@qq.com>
2022-10-12 08:43:34 +0800

    test acion build

--- tools/fetchKubo.js
+++ tools/fetchKubo.js
@@ -1,37 +1,50 @@
-async function fetchzip(path) {
+async function fetchPackage() {
   const target =
     require("os").platform() == "darwin"
-      ? "https://github.com/ipfs/kubo/releases/download/v0.15.0/kubo_v0.15.0_darwin-arm64.tar.gz"
+      ? "https://github.com/ipfs/kubo/releases/download/v0.15.0/kubo_v0.15.0_darwin-amd64.tar.gz"
       : "https://github.com/ipfs/kubo/releases/download/v0.15.0/kubo_v0.15.0_windows-amd64.zip";
-
-  console.log("need fetch", target);
-  const rsp = await require("axios").get(target, {
-    responseType: "stream", // important
-  });
-  await new Promise((resolve) => {
-    rsp.data.pipe(require("fs").createWriteStream(path));
-    rsp.data.on("close", () => {
-      resolve();
+  const localPath = require("path").join(
+    __dirname,
+    require("path").basename(target)
+  );
+  console.log("need fetch", target, "to", localPath);
+  if (!require("fs").existsSync(localPath)) {
+    const rsp = await require("axios").get(target, {
+      responseType: "stream", // important
     });
-  });
+    await new Promise((resolve) => {
+      rsp.data.pipe(require("fs").createWriteStream(localPath));
+      rsp.data.on("close", () => {
+        resolve();
+      });
+    });
+  }
+  return localPath;
 }
 async function run() {
-  if (!require("fs").existsSync("./a.zip")) {
-    await fetchzip("./a.zip");
-  }
-  const AdmZip = require("adm-zip");
-  const zip = new AdmZip("./a.zip");
-  //   var zipEntries = zip.getEntries();
-  //   console.log(zipEntries.map((e) => e.entryName));
-  const target =
+  const localPath = await fetchPackage();
+  const entryPath =
     require("os").platform() == "darwin" ? "kubo/ipfs" : "kubo/ipfs.exe";
-  console.log("need extract", target);
-  zip.extractEntryTo(
-    target,
-    require("path").join(__dirname, "..", "ipfsbin"),
-    false,
-    true
+  const targetDir = require("path").join(__dirname, "..", "ipfsbin");
+  const targetPath = require("path").join(
+    targetDir,
+    require("path").basename(entryPath)
   );
+  if (localPath.endsWith(".zip")) {
+    const AdmZip = require("adm-zip");
+    const zip = new AdmZip("./a.zip");
+    console.log("need extract", entryPath);
+    zip.extractEntryTo(entryPath, targetDir, false, true);
+  } else if (localPath.endsWith(".tar.gz")) {
+    const tar = require("tar");
+    tar.x({
+      file: localPath,
+      filter: (path) => path === entryPath,
+    });
+    if (!require("fs").existsSync(targetPath)) {
+      require("fs").renameSync(entryPath, targetPath);
+    }
+  }
   console.log("done!");
 }
 
